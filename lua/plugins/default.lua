a8dcf688e7dabb6cb241aac13fcf50109b67eb93
Thomas Butler <trbiv@proton.me>
2024-03-14 02:07:31 -0500

    add some more color schemes n shit dawg

--- lua/plugins/default.lua
+++ lua/plugins/default.lua
@@ -12,11 +12,17 @@ return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
 
+  -- add gruvbox material also bc gruvbox is sick
+  { "sainnhe/gruvbox-material"},
+
+  -- add tokyknight 
+  { "folke/tokyonight.nvim" },
+
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "gruvbox-material",
     },
   },
 
@@ -209,6 +215,7 @@ return {
         "shellcheck",
         "shfmt",
         "flake8",
+        "prettier"
       },
     },
   },
