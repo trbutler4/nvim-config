43d661d30b6cb000322e7912088799a09af4a64a
Thomas Butler <trbiv@proton.me>
2024-04-21 14:32:32 -0500

    leymap?

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,11 @@
 
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-
+      dependencies = { 'nvim-lua/plenary.nvim' },
+      keys = {
+        {"<leader>f", "<cmd>Telescope find_files<cr>", desc = "Telescope"},
+      },
+      config = function() 
+          require("telescope").setup()
+      end,
 }
