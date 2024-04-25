8c0239bec30df1f872b9c54b99d64a8c5cfdbffb
Thomas Butler <tombutler4@hotmail.com>
2024-04-25 14:28:51 -0500

    add coq, refactor

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,18 @@
+return {
+    {
+        "neovim/nvim-lspconfig"
+    },
+    {
+        "nvim-treesitter/nvim-treesitter"
+    },
+    {
+        "ms-jpq/coq_nvim"
+    },
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true
+        -- use opts = {} for passing setup options
+        -- this is equalent to setup({}) function
+    },
+}
