3b418d8a808f3436c6d85890671a790c28207f4a
Thomas Butler <trbiv@proton.me>
2024-06-02 20:52:40 -0500

    add setup for language servers to init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- lsp setup
+local lsp = require("lspconfig")
+
+-- Setup language servers.
+lsp.tsserver.setup({})
+lsp.lua_ls.setup({})
+lsp.cairo_ls.setup({
+  cmd = { "scarb", "cairo-language-server", "/C", "--node-ipc" },
+})
+lsp.rust_analyzer.setup({})
+lsp.pyright.setup({})
