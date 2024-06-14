a3ffcb3c6f5712ed47b928ef7239be45283a79d5
TRBIV <trbiv@proton.me>
2024-06-14 18:21:38 -0500

    adding yoga config

--- init.lua
+++ init.lua
@@ -1,46 +1,32 @@
-------------------------------------------------------------------------
--------------------- MISC CONFIG ---------------------------------------
-------------------------------------------------------------------------
 
--- misc
-vim.cmd[[
-    set termguicolors
-    set number 
-    set relativenumber
-]]
+-- ####################################################################################
+-- ################ MISC CONFIGS ######################################################
+-- ####################################################################################
 
--- Set tab width to 4 spaces
-vim.cmd[[
-    set tabstop=4
-    set shiftwidth=4
-    set expandtab
-]]
+-- Set tabs to always be 4 spaces
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
 
--- set leader to space (THIS ISNT WORKING???)
+-- disable netrw 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- leader key is space 
 vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+
+-- line numbers 
+vim.cmd[[set number]]
 
 
--- yank to system clipboard
-vim.cmd[[
-    set clipboard+=unnamedplus
-]]
 
--- for using fzf
-vim.cmd[[
-    set rtp+=/opt/homebrew/opt/fz
-]]
-
--- mouse support 
-vim.cmd[[
-    set mouse=a
-]]
-
-
-------------------------------------------------------------------------
--------------------- LAZY PLUGIN MANAGER -------------------------------
-------------------------------------------------------------------------
--- bootsrap lazy.nvim
+-- ####################################################################################
+-- ################ PLUGIN MANAGER ####################################################
+-- ####################################################################################
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -53,27 +39,148 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
 
--- colorscheme plugin
-vim.cmd[[colorscheme everforest]]
+-- setup plugins 
+require("lazy").setup({
+    'neovim/nvim-lspconfig',
+    'kyazdani42/nvim-tree.lua',
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-vsnip',
+    'hrsh7th/vim-vsnip',
+})
 
+-- file tree setup
+require("nvim-tree").setup()
+vim.keymap.set('n', '<space>t', ':NvimTreeToggle<CR>',  { noremap = true, silent = true })
 
-------------------------------------------------------------------------
--------------------- LSP STUFF -----------------------------------------
-------------------------------------------------------------------------
--- lsp setup 
-local lsp = require('lspconfig')
+-- telescope setup
+require("telescope").setup()
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+--vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- tokyonight setup 
+require("tokyonight").setup()
+vim.cmd[[colorscheme tokyonight-night]]
+
+-- cmp setup
+local cmp = require("cmp")
+cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
+    window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' }, -- For vsnip users.
+    }, {
+      { name = 'buffer' },
+    })
+    })
+
+    -- Set configuration for specific filetype.
+    cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+    }, {
+      { name = 'buffer' },
+    })
+    })
+
+    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+    })
+
+    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+})
+
+-- ####################################################################################
+-- ################ LANGUAGE SERVERS ##################################################
+-- ####################################################################################
+
+-- not currently being used
+local on_attach = function(client)
+    require'completion'.on_attach(client)
+end
 
 -- Setup language servers.
-lsp.tsserver.setup{}
-lsp.lua_ls.setup{}
-lsp.cairo_ls.setup{
-    cmd = { "scarb", "cairo-language-server", "/C", "--node-ipc" }
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local lspconfig = require('lspconfig')
+lspconfig.pyright.setup {
+    capabilities = capabilities
 }
-lsp.rust_analyzer.setup{}
-lsp.pyright.setup{}
-lsp.zls.setup{}
+lspconfig.tsserver.setup {
+    capabilities = capabilities
+}
+lspconfig.cairo_ls.setup {
+    cmd = { "scarb", "cairo-language-server", "/C", "--node-ipc" },
+    capabilities = capabilities
+}
+lspconfig.rust_analyzer.setup {
+    capabilities = capabilities,
+    on_attach=on_attach,
+    settings = {
+        ["rust-analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
+}
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
@@ -112,87 +219,3 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end, opts)
   end,
 })
-
-
-
-------------------------------------------------------------------------
--------------------- AUTOCOMPLETE --------------------------------------
-------------------------------------------------------------------------
-  -- Set up nvim-cmp.
-  local cmp = require'cmp'
-
-  cmp.setup({
-    snippet = {
-      expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body)
-      end,
-    },
-    window = {
-      --completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'vsnip' },
-    }, {
-      { name = 'buffer' },
-    })
-  })
-
-  -- Set configuration for specific filetype.
-  cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-      { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
-    }, {
-      { name = 'buffer' },
-    })
-  })
-
-  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = 'buffer' }
-    }
-  })
-
-  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = 'path' }
-    }, {
-      { name = 'cmdline' }
-    }),
-    matching = { disallow_symbol_nonprefix_matching = false }
-  })
-
-  -- Set up lspconfig.
-  local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-  require('lspconfig').tsserver.setup {
-    capabilities = capabilities
-  }
-  require('lspconfig').lua_ls.setup {
-    capabilities = capabilities
-  }
-  require('lspconfig').cairo_ls.setup {
-    capabilities = capabilities,
-    cmd = { "scarb", "cairo-language-server", "/C", "--node-ipc" }
-  }
-  require('lspconfig').rust_analyzer.setup {
-    capabilities = capabilities,
-  }
-  require('lspconfig').pyright.setup {
-    capabilities = capabilities,
-  }
-  require('lspconfig').zls.setup {
-    capabilities = capabilities,
-  }
--- lua/plugins/colors.lua
+++
@@ -1,6 +0,0 @@
-
-return {
-    "folke/tokyonight.nvim",
-    "sainnhe/gruvbox-material",
-    "sainnhe/everforest"
-}
