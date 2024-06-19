b82c0eaf9c9ebb137a00eb7c2d8ffd204eff182e
Thomas Butler <trbiv@proton.me>
2024-06-18 23:47:18 -0500

    adding toggle term

--- init.lua
+++ init.lua
@@ -58,6 +58,7 @@ require("lazy").setup({
         'nvim-telescope/telescope.nvim', tag = '0.1.2',
         dependencies = { 'nvim-lua/plenary.nvim' },
     },
+    {'akinsho/toggleterm.nvim', version = "*", config = true},
 
     -- colorschemes/visual stuff
     "xiyaowong/transparent.nvim",
@@ -65,10 +66,13 @@ require("lazy").setup({
         "folke/tokyonight.nvim",
         lazy = false,
         priority = 1000,
-        opts = {},
     },
-
-
+    {
+        "neanias/everforest-nvim",
+        lazy = false,
+        priority = 1000,
+        version = false,
+    },
 })
 
 -- file tree setup
@@ -83,9 +87,35 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
--- tokyonight setup 
+-- toggleterm setup 
+require("toggleterm").setup()
+vim.keymap.set('n', '<C-t>', ':ToggleTerm direction=float<CR>',  { noremap = true, silent = true })
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", direction = "float", hidden = true })
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
+-- colorschmes setup 
 require("tokyonight").setup()
-vim.cmd[[colorscheme tokyonight-night]]
+require("everforest").setup()
+vim.cmd[[colorscheme everforest]]
 
 -- cmp setup
 local cmp = require("cmp")
