29386bd2c1ee9d4e28fcbcdca1bf8be8d121c42f
Thomas <trbiv@proton.me>
2024-10-11 22:31:37 -0500

    spellchecking stuff

--- init.lua
+++ init.lua
@@ -18,13 +18,12 @@ vim.opt.termguicolors = true
 -- leader key is space
 vim.g.mapleader = " "
 
--- line numbers
-vim.cmd [[set number]]
-
--- yank to clipboard
-vim.cmd [[set clipboard+=unnamedplus]]
-
-
+vim.cmd [[
+  set number
+  set clipboard+=unnamedplus
+  setlocal spell spelllang=en_us
+  set autoread
+]]
 
 -- ####################################################################################
 -- ################ PLUGINS ###########################################################
@@ -36,7 +35,7 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",     -- latest stable release
+    "--branch=stable", -- latest stable release
     lazypath,
   })
 end
@@ -131,7 +130,7 @@ cmp.setup({
   snippet = {
     -- REQUIRED - you must specify a snippet engine
     expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)       -- For `vsnip` users.
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
       -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
       -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
       -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
@@ -146,11 +145,11 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),     -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'vsnip' },     -- For vsnip users.
+    { name = 'vsnip' }, -- For vsnip users.
   }, {
     { name = 'buffer' },
   })
@@ -159,7 +158,7 @@ cmp.setup({
 -- Set configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
   sources = cmp.config.sources({
-    { name = 'git' },     -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+    { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
   }, {
     { name = 'buffer' },
   })
