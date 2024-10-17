72e2b55e458ba6fa2db116c9041e8b5cdcb09855
Thomas <trbiv@proton.me>
2024-10-16 23:11:35 -0500

    restarting config by cherry picking kickstart

---
+++ lua/plugins/misc.lua
@@ -0,0 +1,11 @@
+return {
+	"tpope/vim-sleuth", -- detect tabstop and shiftwidth automatically
+	{ "numToStr/Comment.nvim", opts = {} }, -- "gc" to comment visual regions/lines
+	-- Highlight todo, notes, etc in comments
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
+}
