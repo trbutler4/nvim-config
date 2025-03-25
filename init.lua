0a9a0fa318a4acc290b5f2fabd9087444f698cd0
Thomas Butler <trbiv@proton.me>
2025-03-25 14:45:41 -0500

    adding new tree

--- init.lua
+++ init.lua
@@ -4,6 +4,10 @@ vim.o.softtabstop = 4
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
 
+-- use nvim tree over netrw
+vim.g.disable_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
@@ -20,14 +24,14 @@ vim.cmd([[
 --
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,5 @@
+return {
+	"nvim-tree/nvim-tree.lua",
+	lazy = false,
+	opts = {},
+}
